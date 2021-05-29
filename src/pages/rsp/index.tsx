99235d8bde20aec7f2d62b32730a4d39e6bdc8f8
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-29 20:03:51 +0900

    battleCountをstateに持たせた

--- src/pages/rsp/index.tsx
+++ src/pages/rsp/index.tsx
@@ -8,17 +8,20 @@ interface State {
   selectHand: HandType;
   enemyHand: HandType;
   resultString: string,
-  winCount: number
+  winCount: number,
+  battleCount: number,
 }
 
 export default class Index extends React.Component<Props, State> {
+  handTypes: HandType[] = [HandType.Rock, HandType.Scissors, HandType.Paper];
   constructor(props: Props) {
     super(props);
     this.state = {
       selectHand: HandType.Rock,
       enemyHand: HandType.Rock,
       resultString: "最初はグーじゃんけん",
-      winCount: 0
+      winCount: 0,
+      battleCount: 0,
     }
   }
 
@@ -27,7 +30,7 @@ export default class Index extends React.Component<Props, State> {
   }
 
   componentDidUpdate(prevPrors: Props, prevState: State) {
-    if(prevState.selectHand !== this.state.selectHand) {
+    if(prevState.battleCount !== this.state.battleCount) {
       const enemyHand: HandType = Math.floor(Math.random()*3);
       this.setState({ enemyHand: enemyHand });
       this.confirmResult(this.state.selectHand, enemyHand);
@@ -35,6 +38,7 @@ export default class Index extends React.Component<Props, State> {
   }
 
   handleOnClick = (val: HandType): void => {
+    this.setState({ battleCount: this.state.battleCount + 1})
     this.setState({selectHand: val});
   }
   confirmResult(selectHand: HandType, enemyHand: HandType): void {
@@ -56,7 +60,6 @@ export default class Index extends React.Component<Props, State> {
   }
 
   render() {
-    const handTypes = [0,1,2]
     return (
       <div
         style={{
@@ -66,9 +69,17 @@ export default class Index extends React.Component<Props, State> {
         }}
       >
         <h1>ジャンケンページ</h1>
+        <h2>自分の手</h2>
         <div style={{ display: "flex" }}>
-          {handTypes.map((handType: HandType) => (
-            <Hand selectHand={handType} selectedHand={this.state.selectHand} clickHand={this.handleOnClick} />
+          {this.handTypes.map((handType: HandType, index: number) => (
+            <Hand selectHand={handType} selectedHand={this.state.selectHand} clickHand={this.handleOnClick} key={index} />
+          ))}
+        </div>
+        <h1 style={{color: "red"}}>{this.state.resultString}</h1>
+        <h2 style={{marginTop: 10}}>相手の手</h2>
+        <div style={{display: "flex"}}>
+          {this.handTypes.map((handType: HandType, index: number) => (
+            <Hand selectHand={handType} selectedHand={this.state.enemyHand} clickHand={this.handleOnClick} key={index}/>
           ))}
         </div>
         <Link to="/">
