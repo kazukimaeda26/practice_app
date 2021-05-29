810985fde8887f89e54f52c54c2fdcc3437eaae5
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-29 10:38:15 +0900

    ジャンケン画像の表示

--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default class Index extends React.Component {
   render() {
+    const imgSize = 100;
     return (
       <div
         style={{
@@ -12,6 +13,38 @@ export default class Index extends React.Component {
         }}
       >
         <h1>ジャンケンページ</h1>
+        <div style={{ display: "flex" }}>
+          <button>
+            <img
+              src={`images/rock.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize
+              }}
+            />
+          </button>
+          <button>
+            <img
+              src={`images/paper.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize
+              }}
+            />
+          </button>
+          <button>
+            <img
+              src={`images/scissors.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize
+              }}
+            />
+          </button>
+        </div>
         <Link to="/">
           <h1>ジャンケンを終了する</h1>
         </Link>
