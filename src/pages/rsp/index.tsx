e4b726931435cbf7aa92e44028356dd03eac29a1
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-29 19:40:07 +0900

    ジャンケンの仕組みを構築,またstateにセット

--- src/pages/rsp/index.tsx
+++ src/pages/rsp/index.tsx
@@ -7,6 +7,8 @@ interface Props {}
 interface State {
   selectHand: HandType;
   enemyHand: HandType;
+  resultString: string,
+  winCount: number
 }
 
 export default class Index extends React.Component<Props, State> {
@@ -15,6 +17,8 @@ export default class Index extends React.Component<Props, State> {
     this.state = {
       selectHand: HandType.Rock,
       enemyHand: HandType.Rock,
+      resultString: "最初はグーじゃんけん",
+      winCount: 0
     }
   }
 
@@ -26,11 +30,29 @@ export default class Index extends React.Component<Props, State> {
     if(prevState.selectHand !== this.state.selectHand) {
       const enemyHand: HandType = Math.floor(Math.random()*3);
       this.setState({ enemyHand: enemyHand });
+      this.confirmResult(this.state.selectHand, enemyHand);
     }
   }
 
   handleOnClick = (val: HandType): void => {
     this.setState({selectHand: val});
+  }
+  confirmResult(selectHand: HandType, enemyHand: HandType): void {
+    const result: number = (selectHand - enemyHand + 3) % 3;
+    if(result === 0){
+      return this.setState({
+        resultString: "あいこで",
+      });
+    } else if (result === 2) {
+      return this.setState({
+        resultString: "勝ち",
+        winCount: this.state.winCount + 1,
+      })
+    } else {
+      return this.setState({
+        resultString: "負け",
+      })
+    }
   }
 
   render() {
