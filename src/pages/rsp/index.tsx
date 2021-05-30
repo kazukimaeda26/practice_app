aee87586f4b3e821954c61a66cedf5cd91ce1a0b
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-30 09:37:54 +0900

    書き換え

--- src/pages/rsp/index.tsx
+++ src/pages/rsp/index.tsx
@@ -1,98 +1,82 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
 import { Link } from "react-router-dom";
 import Hand from "../../components/hand";
 import { HandType } from "../../interfaces/handType";
 
-interface Props {}
-interface State {
-  selectHand: HandType;
-  enemyHand: HandType;
-  resultString: string,
-  winCount: number,
-  battleCount: number,
-}
+export default function Index() {
+  const handTypes: HandType[] = [HandType.Rock, HandType.Scissors, HandType.Paper];
 
-export default class Index extends React.Component<Props, State> {
-  handTypes: HandType[] = [HandType.Rock, HandType.Scissors, HandType.Paper];
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      selectHand: HandType.Rock,
-      enemyHand: HandType.Rock,
-      resultString: "最初はグーじゃんけん",
-      winCount: 0,
-      battleCount: 0,
-    }
-  }
+  const battleCountVal = React.useRef(0);
+  const winCountVal = React.useRef(0);
+  
+  const [selectHand, setSelectHand] = useState<HandType>(HandType.Rock);
+  const [enemyHand, setEnemyHand] = useState<HandType>(HandType.Rock);
+  const [resultString, setResultString] = useState<string>(
+    "最初はグー、ジャンケン"
+  );
+  const [winCount, setWinCount] = useState<number>(0);
+  const [battleCount, setBattleCount] = useState<number>(0);
 
-  componentDidMount() {
+  useEffect(() => {
     alert("手を選ぶと、勝敗が決まるよ。");
-  }
-
-  componentDidUpdate(prevPrors: Props, prevState: State) {
-    if(prevState.battleCount !== this.state.battleCount) {
-      const enemyHand: HandType = Math.floor(Math.random()*3);
-      this.setState({ enemyHand: enemyHand });
-      this.confirmResult(this.state.selectHand, enemyHand);
+    return() => {
+      alert(`【戦績】\n${battleCountVal.current}戦中、${winCountVal.current}勝でした`);
     }
-  }
-  componentWillUnmount() {
-    alert(
-      `【戦績】\n ${this.state.battleCount}戦中、${this.state.winCount}勝でした。`
-    );
-  }
+  },[])
 
-  handleOnClick = (val: HandType): void => {
-    this.setState({ battleCount: this.state.battleCount + 1})
-    this.setState({selectHand: val});
+  useEffect(() => {
+    if( selectHand !== undefined ) {
+      const enemyHand: HandType = Math.floor(Math.random()*3);
+      setEnemyHand(enemyHand);
+      confirmResult(selectHand, enemyHand);
+    }
+    battleCountVal.current = battleCount;
+    winCountVal.current = winCount;
+  }, [battleCount]);
+
+  const handleOnClick = (val: HandType): void => {
+    setBattleCount(battleCount + 1);
+    setSelectHand(val);
   }
-  confirmResult(selectHand: HandType, enemyHand: HandType): void {
+  const confirmResult = (selectHand: HandType, enemyHand: HandType): void => {
     const result: number = (selectHand - enemyHand + 3) % 3;
     if(result === 0){
-      return this.setState({
-        resultString: "あいこで",
-      });
+      setResultString("あいこで");
     } else if (result === 2) {
-      return this.setState({
-        resultString: "勝ち",
-        winCount: this.state.winCount + 1,
-      })
+      setResultString("勝ち");
+      setWinCount(winCount+1);
     } else {
-      return this.setState({
-        resultString: "負け",
-      })
+      setResultString("負け");
     }
   }
 
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column"
-        }}
-      >
-        <h1>ジャンケンページ</h1>
-        <h2>自分の手</h2>
-        <div style={{ display: "flex" }}>
-          {this.handTypes.map((handType: HandType, index: number) => (
-            <Hand selectHand={handType} selectedHand={this.state.selectHand} clickHand={this.handleOnClick} key={index} />
-          ))}
-        </div>
-        <h1 style={{color: "red"}}>{this.state.resultString}</h1>
-        <h2 style={{marginTop: 10}}>相手の手</h2>
-        <div style={{display: "flex"}}>
-          {this.handTypes.map((handType: HandType, index: number) => (
-            <Hand selectHand={handType} selectedHand={this.state.enemyHand} clickHand={this.handleOnClick} key={index}/>
-          ))}
-        </div>
-        <Link to="/">
-          <h1>ジャンケンを終了する</h1>
-          <h4>戦績が発表されます。</h4>
-        </Link>
-
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <h1>ジャンケンページ</h1>
+      <h2>自分の手</h2>
+      <div style={{ display: "flex" }}>
+        {handTypes.map((handType: HandType, index: number) => (
+          <Hand selectHand={handType} selectedHand={selectHand} clickHand={handleOnClick} key={index} />
+        ))}
       </div>
-    )
-  }
+      <h1 style={{color: "red"}}>{resultString}</h1>
+      <h2 style={{marginTop: 10}}>相手の手</h2>
+      <div style={{display: "flex"}}>
+        {handTypes.map((handType: HandType, index: number) => (
+          <Hand selectHand={handType} selectedHand={enemyHand} clickHand={handleOnClick} key={index}/>
+        ))}
+      </div>
+      <Link to="/">
+        <h1>ジャンケンを終了する</h1>
+        <h4>戦績が発表されます。</h4>
+      </Link>
+
+    </div>
+  )
 }
